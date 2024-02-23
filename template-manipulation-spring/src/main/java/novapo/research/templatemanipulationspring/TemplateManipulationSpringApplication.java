9560bdddc9d3dcfd965384d6b77bc98824b1ad53
Ngo Phong <phongngonovapo@gmail.com>
2024-02-23 11:38:25 +0700

    init source

---
+++ template-manipulation-spring/src/main/java/novapo/research/templatemanipulationspring/TemplateManipulationSpringApplication.java
@@ -0,0 +1,13 @@
+package novapo.research.templatemanipulationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateManipulationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateManipulationSpringApplication.class, args);
+	}
+
+}
---
+++ template-manipulation-spring/src/test/java/novapo/research/templatemanipulationspring/TemplateManipulationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package novapo.research.templatemanipulationspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateManipulationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
